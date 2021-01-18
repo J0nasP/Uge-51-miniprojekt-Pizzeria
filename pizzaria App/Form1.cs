f89fe978ec639e1b9e3bbeb9c9aa3d573ecd2611
J0nasP <petersen0108@gmail.com>
2021-01-18 14:42:41 +0100

    Total pris added to soda aswell

--- pizzaria App/Form1.cs
+++ pizzaria App/Form1.cs
@@ -130,9 +130,36 @@ namespace pizzaria_App
             int pizza_total_number = skinke + peperoni + salat + fantasi;
             total_pizza_label.Text = pizza_total_number.ToString() + " kr";
         }
-        
 
+        #region udregning af pris på sodavand
+        private int udregning_cola(Label cola_33, Label cola_50, Label cola_150)
+        {
+            int total_pris_cola = Convert.ToInt32(cola_33.Text) + Convert.ToInt32(cola_50.Text) + Convert.ToInt32(cola_150.Text);
+            return total_pris_cola;
+        }
 
+        private int udregning_fanta(Label fanta_33, Label fanta_50, Label fanta_150)
+        {
+            int total_pris_fanta = Convert.ToInt32(fanta_33.Text) + Convert.ToInt32(fanta_50.Text) + Convert.ToInt32(fanta_150.Text);
+            return total_pris_fanta;
+        }
+
+        private int udregning_sprite(Label sprite_33, Label sprite_50, Label sprite_150)
+        {
+            int total_pris_sprite = Convert.ToInt32(sprite_33.Text) + Convert.ToInt32(sprite_50.Text) + Convert.ToInt32(sprite_150.Text);
+            return total_pris_sprite;
+        }
+
+        private void udregning_soda_total(Label total_drikkevarer_label)
+        {
+            int cola = udregning_cola(cola_33_pris, cola_50_pris, cola_150_pris);
+            int fanta = udregning_fanta(fanta_33_pris, fanta_50_pris, fanta_150_pris);
+            int sprite = udregning_sprite(sprite_33_pris, sprite_50_pris, sprite_150_pris);
+
+            int soda_total_number = cola + fanta + sprite;
+            total_drikkevarer_label.Text = soda_total_number.ToString() + " kr";
+        }
+        #endregion
 
         #endregion
 
@@ -152,19 +179,19 @@ namespace pizzaria_App
         private void udregning_lille_soda(NumericUpDown antal, Label pris_total)
         {
             decimal value = antal.Value * 15;
-            pris_total.Text = value.ToString() + " kr";
+            pris_total.Text = value.ToString();
         }
 
         private void udregning_mellem_soda(NumericUpDown antal, Label pris_total)
         {
             decimal value = antal.Value * 20;
-            pris_total.Text = value.ToString() + "kr";
+            pris_total.Text = value.ToString();
         }
 
         private void udregning_stor_soda(NumericUpDown antal, Label pris_total)
         {
             decimal value = antal.Value * 30;
-            pris_total.Text = value.ToString() + "kr";
+            pris_total.Text = value.ToString();
         }
 
         #endregion
@@ -266,6 +293,11 @@ namespace pizzaria_App
             udregning_stor_soda(antal_sprite_150, sprite_150_pris);
         }
 
+        private void udregning_soda_total_label()
+        {
+            udregning_soda_total(total_drikkevarer_pris);
+        }
+
         #endregion
 
 
@@ -324,46 +356,55 @@ namespace pizzaria_App
         private void udregning_lille_cola_button(object sender, EventArgs e)
         {
             udregning_cola_lille_label();
+            udregning_soda_total_label();
         }
 
         private void udregning_mellem_cola_button(object sender, EventArgs e)
         {
             udregning_cola_mellem_label();
+            udregning_soda_total_label();
         }
 
         private void udregning_stor_cola_button(object sender, EventArgs e)
         {
             udregning_cola_stor_label();
+            udregning_soda_total_label();
         }
 
         private void udregning_lille_sprite_button(object sender, EventArgs e)
         {
             udregning_sprite_lille();
+            udregning_soda_total_label();
         }
 
         private void udregning_mellem_sprite_button(object sender, EventArgs e)
         {
             udregning_sprite_mellem();
+            udregning_soda_total_label();
         }
 
         private void udregning_stor_sprite_button(object sender, EventArgs e)
         {
-            udregning_sprite_stor();        
+            udregning_sprite_stor();
+            udregning_soda_total_label();
         }
 
         private void udregning_lille_fanta_button(object sender, EventArgs e)
         {
             udregning_fanta_lille_label();
+            udregning_soda_total_label();
         }
 
         private void udregning_mellem_fanta_button(object sender, EventArgs e)
         {
             udregning_fanta_mellem_label();
+            udregning_soda_total_label();
         }
 
         private void udregning_stor_fanta_button(object sender, EventArgs e)
         {
             udregning_fanta_stor();
+            udregning_soda_total_label();
         }
         #endregion
     }
